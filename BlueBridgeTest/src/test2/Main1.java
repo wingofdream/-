56c579eb4e5154bffac6b9e3701527b9016f8952
Lemon <Lemon@DESKTOP-D406OTH>
2019-06-18 22:16:56 +0800

    demo

--- BlueBridgeTest/src/test2/Main1.java
+++ BlueBridgeTest/src/test2/Main1.java
@@ -2,6 +2,6 @@ package test2;
 
 public class Main1 {
 	public static void main(String[] args) {
-		System.out.println("asdjasifhasjhfasfh ");
+		System.out.println("asdjasifhasjhfasfh ");//jbjghv
 	}
 }
